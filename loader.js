4f45bd96693e0243670f108c0a07785f667833e0
league-of-assassins <league-of-assassins@users.noreply.github.com>
2018-07-31 15:46:24 +0300

    1

--- loader.js
+++
@@ -1,8 +0,0 @@
-if (!sessionStorage.isVisited) {
-  sessionStorage.isVisited = 'true'
-  $("#status").delay(1500).fadeOut("slow")
-  $("#preloader").delay(2000).fadeOut("slow")
-} else {
-  $("#status").hide()
-  $("#preloader").hide()
-}
