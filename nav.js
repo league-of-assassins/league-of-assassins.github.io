cb1a0935ddac023b7d17778ebcc700e4664b7337
league-of-assassins <league-of-assassins@users.noreply.github.com>
2018-08-02 14:21:46 +0300

    1

--- nav.js
+++ nav.js
@@ -1,9 +1,10 @@
+
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("topnav").style.top = "0";
-  } else {
-    document.getElementById("topnav").style.top = "-50px";
-  }
-} 
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        document.getElementById("topnav").style.top = "0";
+    } else {
+        document.getElementById("topnav").style.top = "0";
+    }
+}
