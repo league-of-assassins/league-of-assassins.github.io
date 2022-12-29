69da098afac864a405604b82f2e3dafe31585a8d
league-of-assassins <easymailjw2@gmail.com>
2022-12-29 09:41:55 +0300

    improvements

--- js/main.js
+++ js/main.js
@@ -1,45 +1,27 @@
-	//								TOPNAV SCROLL ON CLICK START
+	//								TOPNAV SCROLL ON CLICK
 
 function scrollWin1() {window.scrollTo({top: 0, behavior: "smooth"});}
 function scrollWin2() {window.scrollTo({top: 820, behavior: "smooth"});}
 function scrollWin3() {window.scrollTo({top: 1190, behavior: "smooth"});}
-function scrollWin4() {window.scrollTo({top: 1690, behavior: "smooth"});}
+function scrollWin4() {window.scrollTo({top: 1750, behavior: "smooth"});}
 
-	//								TOPNAV SCROLL ON CLICK END
 
-	//								OPEN/CLOSE IMAGE ON CLICK START
+	//								OPEN/CLOSE IMAGE ON CLICK
 
-var modal = document.getElementById('myModal'); var img = document.getElementById("img1");
-var modalImg = document.getElementById("modal-img"); var captionText = document.getElementById("caption");
-img.onclick = function(){modal.style.display = "block"; modalImg.src = this.src; captionText.innerHTML = this.alt;}
-var span = document.getElementsByClassName("close")[0]; span.onclick = function() { modal.style.display = "none";}
+var modal = document.getElementById('myModal');
+var images = document.getElementsByClassName('galleryimages');
+var modalImg = document.getElementById("modal-img");
+var captionText = document.getElementById("caption");
 
-var img = document.getElementById("img2");
-img.onclick = function(){modal.style.display = "block"; modalImg.src = this.src; captionText.innerHTML = this.alt;}
-span.onclick = function() { modal.style.display = "none";}
+for (var i = 0; i < images.length; i++) {
+  var img = images[i];
+  img.onclick = function(evt) {
+    console.log(evt);
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+}
 
-var img = document.getElementById("img3");
-img.onclick = function(){modal.style.display = "block"; modalImg.src = this.src; captionText.innerHTML = this.alt;}
-span.onclick = function() { modal.style.display = "none";}
-
-var img = document.getElementById("img4");
-img.onclick = function(){modal.style.display = "block"; modalImg.src = this.src; captionText.innerHTML = this.alt;}
-span.onclick = function() { modal.style.display = "none";}
-
-var img = document.getElementById("img5");
-img.onclick = function(){modal.style.display = "block"; modalImg.src = this.src; captionText.innerHTML = this.alt;}
-span.onclick = function() { modal.style.display = "none";}
-
-var img = document.getElementById("img6");
-img.onclick = function(){modal.style.display = "block"; modalImg.src = this.src; captionText.innerHTML = this.alt;}
-span.onclick = function() { modal.style.display = "none";}
-
-var img = document.getElementById("img7");
-img.onclick = function(){modal.style.display = "block"; modalImg.src = this.src; captionText.innerHTML = this.alt;}
-span.onclick = function() { modal.style.display = "none";}
-
-var img = document.getElementById("img8");
-img.onclick = function(){modal.style.display = "block"; modalImg.src = this.src; captionText.innerHTML = this.alt;}
-span.onclick = function() { modal.style.display = "none";}
-
-	//									OPEN/CLOSE IMAGE ON CLICK END
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {modal.style.display = "none";}
