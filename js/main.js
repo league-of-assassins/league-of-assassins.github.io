3910f63b0f472446571715c07c8beb89e299b747
league-of-assassins <easymailjw2@gmail.com>
2022-12-29 20:11:24 +0300

    gallery changes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 	//								TOPNAV SCROLL ON CLICK
 
 function scrollWin1() {window.scrollTo({top: 0, behavior: "smooth"});}
-function scrollWin2() {window.scrollTo({top: 810, behavior: "smooth"});}
-function scrollWin3() {window.scrollTo({top: 1290, behavior: "smooth"});}
-function scrollWin4() {window.scrollTo({top: 1950, behavior: "smooth"});}
+function scrollWin2() {window.scrollTo({top: 800, behavior: "smooth"});}
+function scrollWin3() {window.scrollTo({top: 1260, behavior: "smooth"});}
+function scrollWin4() {window.scrollTo({top: 1940, behavior: "smooth"});}
 
 
 	//								OPEN/CLOSE IMAGE ON CLICK
